7b579e58d6013fbcfa8d2d97c3fc214645181eba
Kushank Maheshwari <f20180679@pilani.bits-pilani.ac.in>
2020-08-18 20:21:05 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,3 @@
+var one = document.getElementById('height');
+var two = document.getElementById('sec');
+document.write('<a href="mailto:esafe@esafe.co.in?subject=Inquiry%20-%20Fibre%20Glass%20Double%20Step%20 Ladder&body=We%20want%20Fibre%20Glass%20Cage%20Ladder%20with%20folowing%20speifications:'+one+'">email</a>');
